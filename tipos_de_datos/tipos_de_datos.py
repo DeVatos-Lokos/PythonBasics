133431b73d86111dc66f734a43b40e77dccc6430
Iván Pineda <ivan@codster.io>
2021-07-06 22:43:37 -0500

    test no resueltos

---
+++ tipos_de_datos/tipos_de_datos.py
@@ -0,0 +1,119 @@
+# En esta practica entendéremos los tipos de datos básicos en python, y realizaremos
+# algunas de sus operaciones básicas
+
+####################################################################################
+####################################################################################
+#########TODO LO QUE COMIENZE CON # O ESTÉ ENTRE """""" ES UN COMENTARIO############
+###########PYTHON NO LO ITERPRETARÁ CÓMO CÓDIGO, SOLO SON EXPLICACIONES#############
+####################################################################################
+####################################################################################
+
+
+class TiposDeDatos:
+    def __init__(self):
+        # Revisa el ejemplo de es boolean y utiliza las variables declaradas abajo
+        # para resolver los ejercicios
+        self.tipo_string = ""
+        self.tipo_bool = False
+        self.tipo_int = 0
+        self.tipo_float = 0.0
+        self.tipo_byte = b''
+        self.tipo_list = []
+
+    def es_string(self, recibimos): #Ejemplo
+        """
+        El tipo de dato string, es una cadena de carácteres, como una palabra o una letra,
+        se expresa con "" comillas, otros tipos de datos puede convertirse en string, como un número,
+        bastara expresarlo con comillas, p. ej "1".
+        Este método recibe parámetro s, regresa verdadero si es de tipo string o falso si no lo es
+
+        """
+        # Para saber el tipo de un dato es necesario invocar el método type
+        # Un método no necesariamente debe retornar (return) algo, pero es muy útil
+        # saber usar return, en español la línea de abajo se leería de la sig manera
+        # Retorna si el tipo de dato que recibimos es igual al tipo de dato de self.tipo string que es string
+        # Si son iguales == es verdadero, si no es falso, regresamos True o False
+        return type(self.tipo_string) == type(recibimos)
+
+
+    def es_boolean(self, recibimos): #Ejemplo
+        """
+        El tipo boolean puede ser vedadero (True) o falso (False), este méetodo,
+        nos regresará True si es boolean y false, si no lo es
+        """
+        pass # Borra pass y aquí escribe tú código
+
+    def es_int(self, recibimos): # Ejercicio
+        """
+        EL tipo de dato int, es un número entero, puede ser negativo o positivo
+        """
+        pass # Borra pass y aquí escribe tú código
+
+    def es_float(self, recibimos): # Ejercicio
+        """
+        El tipo de dato float es un número décimal
+        """
+        pass # Borra pass y aquí escribe tú código
+
+    def es_byte(self, recibimos): # Ejercicio
+        """
+        El tipo de dato byte es una cadena de bytes, es el tipo más básico en cualquier lenguaje de programación,
+        todos los demás tipos de datos pueden transformarse en este
+        """
+        pass # Borra pass y aquí escribe tú código
+
+    def es_list(self, recibimos):
+        """
+        El tipo de dato lista, es un arreglo que puede contener otros tipos de datos, 
+        una lista de objetos (Sting, List, Tuple, Clases) o tipos primitivos (int, byte, float).
+        Es una estructura de datos, la más básica de todas y probablemente la que más usaran,
+        en python un String es, d hecho una lista de carácteres.
+        """
+        pass # Borra pass y aquí escribe tú código
+
+    # DECLARACIÓN DE VARIABLES Y ASIGNACIONES
+    def regresa_un_string(self): # Ejemplo
+        return "Hola mundo"
+
+    def regresa_un_boolean(self):
+        return # Completa
+
+    def regresa_un_int(self):
+        return # Completa
+
+    def regresa_un_float(self):
+        return # Completa
+
+    def regresa_un_byte(self):
+        return # Completa
+
+    def regresa_una_lista(self):
+        return # Completa
+
+    def regresa_una_lista_de_longitud_10(self):
+        return # Completa
+
+    # CONVERSIONES DE DATOS
+    def to_string(self, recibimos): #Ejemplo
+        """
+        Es una de las conversiones (casting en inglés) más comunes, consiste en generar la
+        representación en cádena de texto de un objeto, en python todo es un objeto, es el tipo
+        básico en los lenguajes con orientación a objetos, lo verémos más adelante.
+        El método __str__ que se llama con str() es el encargado de hacer dicho proceso por default
+        en python
+        """
+        return str(recibimos)
+
+    def int_to_float(self, recibimos):
+        """
+        La conversión de un entero a float solo agrega decimales al entero, 0 se convierte en 0.0,
+        1 en 1.0 y así sucesivamente
+        """
+        pass # Borra pass y aquí escribe tú código
+
+    def float_to_int(self, recibimos):
+        """
+        La conversión de float a entero le quita decimales, se pierde información, un 3.9, se convierte en 3,
+        3.1 en 3.
+        """
+        pass # Borra pass y aquí escribe tú código
---
+++ tipos_de_datos/tipos_de_datos_test.py
@@ -0,0 +1,158 @@
+import unittest
+import random
+
+from tipos_de_datos import TiposDeDatos
+
+class TestTiposDeDatos(unittest.TestCase):
+
+    def setUp(self):
+        self.tipos_de_datos = TiposDeDatos()
+
+    def test_es_string(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_string("Hola mundo")
+        )
+        falsos = [1, 1.0, [], True, False, b""]
+        for f in falsos:
+            self.assertFalse(
+                self.tipos_de_datos.es_string(f)
+            )
+
+    def test_es_boolean(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_boolean(True)
+        )
+        self.assertTrue(
+            self.tipos_de_datos.es_boolean(False)
+        )
+        falsos = [1, 1.0, [], "Hola mundo", b""]
+        for f in falsos:
+            self.assertFalse(
+                self.tipos_de_datos.es_boolean(f)
+            )
+
+    def test_es_int(self):
+        for i in range(10):
+            self.assertTrue(
+                self.tipos_de_datos.es_int(i)
+            )
+        falsos = ["Hola mundo", 1.0, [], True, False, b""]
+        for f in falsos:
+            self.assertFalse(
+                self.tipos_de_datos.es_int(f)
+            )
+
+    def test_es_float(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_float(1.0)
+        )
+        self.assertTrue(
+            self.tipos_de_datos.es_float(2.0)
+        )
+        falsos = [1, [], True, False, b""]
+        for f in falsos:
+            self.assertFalse(
+                self.tipos_de_datos.es_float(f)
+            )
+
+    def test_es_byte(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_byte(b"")
+        )
+        falsos = [1, 1.0, [], True, False, "Hola mundo"]
+        for f in falsos:
+            self.assertFalse(
+                self.tipos_de_datos.es_byte(f)
+            )
+
+    def test_es_list(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_list([])
+        )
+        falsos = [1, 1.0, True, False, "Hola mundo"]
+        for f in falsos:
+            self.assertFalse(
+                self.tipos_de_datos.es_list(f)
+            )
+        
+    def test_regresa_un_string(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_string(
+                self.tipos_de_datos.regresa_un_string()
+            )
+        )
+
+    def test_regresa_un_boolean(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_boolean(
+                self.tipos_de_datos.regresa_un_boolean()
+            )
+        )
+
+    def test_regresa_un_int(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_int(
+                self.tipos_de_datos.regresa_un_int()
+            )
+        )
+
+    def test_regresa_un_float(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_float(
+                self.tipos_de_datos.regresa_un_float()
+            )
+        )
+
+    def test_regresa_un_byte(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_byte(
+                self.tipos_de_datos.regresa_un_byte()
+            )
+        )
+
+    def test_regresa_una_lista(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_lista(
+                self.tipos_de_datos.regresa_una_lista()
+            )
+        )
+
+    def regresa_una_lista_de_longitud_10(self):
+        self.assertTrue(
+            self.tipos_de_datos.es_lista(
+                self.tipos_de_datos.regresa_una_lista_de_longitud_10()
+            )
+        )
+        self.assertTrue(
+            len(
+                self.tipos_de_datos.regresa_una_lista_de_longitud_10()
+            ) == 10
+        )
+
+    def test_to_string(self):
+        for t in [1, 1.0, "Hola mundo", True, False]:
+            self.assertTrue(
+                self.tipos_de_datos.es_string(
+                    self.tipos_de_datos.to_string(
+                        t
+                    )
+                )
+            )
+
+    def test_int_to_float(self):
+        r = random.randint(-100, 100)
+        self.assertEquals(
+            float(r), 
+            self.tipos_de_datos.int_to_float(r)
+        )
+
+    def test_float_to_int(self):
+        r = random.randint(-100, 100)
+        r_f = random.randint(1, 100) / 100
+        self.assertEquals(
+            int(r + r_f), 
+            self.tipos_de_datos.float_to_int(r + r_f)
+        )
+
+if __name__ == '__main__':
+    unittest.main()
