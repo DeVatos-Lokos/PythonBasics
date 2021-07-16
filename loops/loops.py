7ab4c5771082f822614eeccb2fa7ac88929cad3c
Iván Pineda <ivan@codster.io>
2021-07-16 11:43:48 -0500

    loops comentada

--- loops/loops.py
+++ loops/loops.py
@@ -3,39 +3,63 @@ class NaturalNumbers:
         pass
 
     def get_first_n_for(self, n): # Ejemplo
-        first_n = []
-        for i in range(n):
-            first_n.append(i)
+        """
+        Obtener los primeros n naturales en una lista con for
+        """
+        first_n = [] # Se declara una lista donde almacenaremos los numeros
+        for i in range(n): # Se itera sobre range que genera un rango de 0 a n
+            first_n.append(i) # Almacenamos la variable del ciclo en la lista con append 
         print("FIRST n (n={}) FOR: {}".format(n, first_n))
-        return first_n
+        return first_n # Regresamos la lista
 
     def get_first_n_while(self, n): # Ejemplo
-        first_n = []
-        n_count = 0
-        while n_count < n:
-            first_n.append(n_count)
-            n_count += 1
+        """
+        Obtener los primeros n naturales en una lista con while
+        """
+        first_n = [] # Se declara una lista donde almacenaremos los numeros
+        n_count = 0 # Inicializamos un contador para saber en que iteracion vamos dentro del ciclo
+        while n_count < n: # Condición de terminación del ciclo 
+            first_n.append(n_count) # ALmacenamos el contador (contablizador del ciclo) en la lista
+            n_count += 1 # Sumamos uno al contador puesto que termina ek ciclo, si no nunca n_count será mayor o igual que n y tendremos un loop infinito
         print(f"FIRST n (n={n}) WHILE: {first_n}")
         return first_n
 
     def get_first_n_pair_for(self, n):
+        """
+        Obtener los primeros n pares en una lista con for
+        """
         return [i for i in range(0, n, 2)]
 
     def get_first_n_pair_while(self, n):
+        """
+        Obtener los primeros n pares en una lista con while
+        """
         return [i for i in range(0, n, 2)]
 
     def get_factorial_for(self, n):
+        """
+        Obtener el factorial de n con for, regresa un int
+        """
         return self.get_factorial_recursive(n)
 
     def get_factorial_while(self, n):
+        """
+        Obtener el factorial de n con while, regresa un int
+        """
         return self.get_factorial_recursive(n)
 
     def get_factorial_recursive(self, n): #Ejemplo
+        """
+        Obtener el factorial de n recursivamente, regresa un int
+        """
         if n <= 1:
             return 1
         return n * self.get_factorial_recursive(n-1)
 
     def get_n_pow_2_for(self, n):
+        """
+        Obtener el cuadrado de los primeros n con for, regresa una lista
+        """
         n_pow_2 = []
         for i in range(n):
             n_pow_2.append(
@@ -45,15 +69,27 @@ class NaturalNumbers:
         return n_pow_2
 
     def get_n_pow_2_while(self, n):
+        """
+        Obtener el cuadrado de los primeros n con while, regresa una lista
+        """
         return self.get_n_pow_2_for(n)
 
     def get_n_sum_recursive(self, n): #Ejemplo
+        """
+        Obtener la suma de los primeros n recursivamente, regresa un int
+        """
         if n <= 0:
             return 0
         return n + self.get_n_sum_recursive(n-1)
 
     def get_n_sum_for(self, n):
+        """
+        Obtener la suma de los primeros n con for, regresa un int
+        """
         return self.get_n_sum_recursive(n)
 
     def get_n_sum_while(self, n):
+        """
+        Obtener la suma de los primeros n con while, regresa un int
+        """
         return self.get_n_sum_recursive(n)
